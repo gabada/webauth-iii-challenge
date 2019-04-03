951cd6ef01b16c2a640ab696b9883604302e3cd7
Gill Abada <gill@callifi.com>
2019-04-03 18:03:06 -0400

    add restricted middleware.

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const secrets = require('../api/secrets.js').jwtSecret;
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secrets, (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({ message: 'You shall not pass!' });
+      } else {
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: 'no token provided' });
+  }
+};
