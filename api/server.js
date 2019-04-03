c1967792e41d36a1f933c0cb3d93b25a24c5d526
Gill Abada <gill@callifi.com>
2019-04-03 16:12:43 -0400

    add router, add bcryptjs dependency.

--- api/server.js
+++ api/server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const authRouter = require('../auth/auth-router.js');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/auth', authRouter);
 
 module.exports = server;
