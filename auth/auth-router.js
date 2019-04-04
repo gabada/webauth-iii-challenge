a8a934fac9227f9508a90e2da3b3949c71344cb7
Gill Abada <gill@callifi.com>
2019-04-04 08:06:05 -0400

    refactor token service.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,21 +1,8 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const tokensService = require('../auth/token-service.js');
 
 const Users = require('../users/users-models.js');
-const secret = require('../api/secrets.js').jwtSecret;
-
-const generateToken = user => {
-  const payload = {
-    subject: user.id,
-    username: user.username,
-    department: user.department
-  };
-  const options = {
-    expiresIn: '1d'
-  };
-  return jwt.sign(payload, secret, options);
-};
 
 router.post('/register', (req, res) => {
   let user = req.body;
@@ -38,7 +25,7 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user);
+        const token = tokensService.generateToken(user);
         res.status(200).json({ message: 'Welcome!', token });
       } else {
         res.status(401).json({ message: 'You shall not pass!' });
